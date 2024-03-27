c2a0faae12e513a5bb25e984cd293689e4b08ca2
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-03-27 19:22:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <router-link v-bind:to="{ name: 'Home' }">Home</router-link> |
+    <router-link v-bind:to="{ name: 'About' }">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<script setup></script>
